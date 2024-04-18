e9ff12c49cad4287dbd4a66bc793a6c53fabf391
Nabeel Ahmed <nabeelahmedkhan999@gmail.com>
2024-04-18 08:06:04 +0500

    nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 export default function Header() {
   return (
-    <div className="flex justify-between items-center flex-col md:flex-row pr-8 border-b">
+    <div className="flex justify-between items-center flex-col md:flex-row pr-0 md:pr-8 border-b">
       <Logo />
       <Navbar />
     </div>
