af9d316a3abf52dbf9876790e974ae78704acd47
Nabeel Ahmed <nabeelahmedkhan999@gmail.com>
2024-04-18 08:01:50 +0500

    css

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -3,21 +3,21 @@ import NewModal from "./NewModal";
 const ImageGallery = () => {
   return (
     <>
-      <div className="flex justify-center gap-8 pt-12">
-        <span className="text-gray-800 font-bold hover:text-orange-500 transition-all hover:cursor-pointer uppercase">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-8 pt-12">
+        <span className="text-gray-800 font-bold md:font-semibold hover:text-orange-500 transition-all hover:cursor-pointer uppercase">
           Show All
         </span>
-        <span className="text-gray-800 font-bold hover:text-orange-500 transition-all hover:cursor-pointer uppercase">
+        <span className="text-gray-800 font-bold md:font-semibold hover:text-orange-500 transition-all hover:cursor-pointer uppercase">
           First Category
         </span>
-        <span className="text-gray-800 font-bold hover:text-orange-500 transition-all hover:cursor-pointer uppercase">
+        <span className="text-gray-800 font-bold md:font-semibold hover:text-orange-500 transition-all hover:cursor-pointer uppercase">
           Second Category
         </span>
-        <span className="text-gray-800 font-bold hover:text-orange-500 transition-all hover:cursor-pointer uppercase">
+        <span className="text-gray-800 font-bold md:font-semibold hover:text-orange-500 transition-all hover:cursor-pointer uppercase">
           Third Category
         </span>
       </div>
-      <section className="grid grid-cols-1 md:grid-cols-4 gap-4 py-8">
+      <section className="grid grid-cols-1 place-items-center md:grid-cols-4 gap-4 py-8">
         <NewModal src="/gallery-img01-bg.webp" alt="gallary 1" />
         <NewModal src="/gallery-img02-bg.webp" alt="gallary 2" />
         <NewModal src="/gallery-img08-bg.webp" alt="gallary 8" />
